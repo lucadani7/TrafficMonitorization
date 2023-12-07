4ef23597f516b0951c049e65592dca13c0623a5a
lucadani7 <lucaionescu1998@gmail.com>
2023-12-07 10:06:55 +0200

    Initial commit

---
+++ src/main/java/com/lucadani/trafficmonitorization/TrafficMonitorizationApplication.java
@@ -0,0 +1,13 @@
+package com.lucadani.trafficmonitorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficMonitorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficMonitorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucadani/trafficmonitorization/TrafficMonitorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucadani.trafficmonitorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficMonitorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
