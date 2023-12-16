178f0db09d5bed62c361eb350a8789bc51db6c0e
lucadani7 <lucaionescu1998@gmail.com>
2023-12-16 15:59:30 +0200

    App proprieties written

--- src/main/java/com/lucadani/trafficmonitorization/TrafficMonitorizationApplication.java
+++ src/main/java/com/lucadani/trafficmonitorization/TrafficMonitorizationApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrafficMonitorizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrafficMonitorizationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficMonitorizationApplication.class, args);
+    }
 }
