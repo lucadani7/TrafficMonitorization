5a12dafad681be877c48de9a9d8524ef4225ca39
lucadani7 <lucaionescu1998@gmail.com>
2023-12-16 16:28:47 +0200

    DataNotFoundException class written

---
+++ src/main/java/com/lucadani/trafficmonitorization/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lucadani.trafficmonitorization.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
